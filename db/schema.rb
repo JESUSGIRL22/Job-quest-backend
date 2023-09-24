821465f50f3eb6b826e21e8a545833662802f4f4
techlover <e.rono@outlook.com>
2023-09-24 23:15:30 +0300

    UserProfiles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_24_200922) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_24_201437) do
   create_table "job_listings", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,6 +18,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_24_200922) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "user_profiles", force: :cascade do |t|
+    t.string "name"
+    t.text "bio"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +41,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_24_200922) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "user_profiles", "users"
 end
