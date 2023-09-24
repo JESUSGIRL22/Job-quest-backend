9017036a8db1102a4f9a69e0fe4d17bb4411e530
techlover <e.rono@outlook.com>
2023-09-24 23:18:49 +0300

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :user_profiles
+  resources :user_profiles, only: [:show, :edit, :update, :destroy]
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
