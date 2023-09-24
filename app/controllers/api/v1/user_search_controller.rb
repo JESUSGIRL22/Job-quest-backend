0bfe62ed8d7452b323a2ed174609bc111c23a844
techlover <e.rono@outlook.com>
2023-09-24 23:37:02 +0300

    updated user-search-controller

--- app/controllers/api/v1/user_search_controller.rb
+++ app/controllers/api/v1/user_search_controller.rb
@@ -1,2 +1,14 @@
-class Api::V1::UserSearchController < ApplicationController
+module Api
+  module V1
+    class UserSearchController < ApplicationController
+      def index
+        name = params[:name]
+
+        # Perform user search based on the name parameter
+        @results = User.where("lower(name) LIKE ?", "%#{name.downcase}%")
+
+        render json: @results
+      end
+    end
+  end
 end
